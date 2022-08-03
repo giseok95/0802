2e8b65d3fc1a29c7df57d81181387351a846b76d
giseok <105585892+giesoek@users.noreply.github.com>
2022-08-03 09:26:20 +0900

    점수채점 예제

--- src/Practice.java
+++ src/Practice.java
@@ -1,18 +1,38 @@
 
 public class Practice {
 	public static void main(String[] args) {
-		int jimin[]= {1,2,2,4,2,2,3,4,1,2};
-		result(jimin);
-	}
+		int answer[] = { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2 };
+		String names[] = { "한지민", "박지민", "홍지민", "신지민", "김지민" };
 
-	static void result(int[] student) {
-		int result[] = {1,2,3,4,1,2,3,4,1,2 };
-		for (int i = 0; i <= result.length; i++) {
-			if (result[i] == student[i])
-				System.out.print("o\t");
-			else
-				System.out.print("x\t");
+		int array[][] = { 
+				{ 1, 2, 2, 4, 2, 2, 3, 4, 1, 2 }, 
+				{ 1, 2, 3, 4, 1, 1, 1, 4, 1, 1 },
+				{ 1, 2, 3, 4, 1, 2, 3, 4, 1, 2 }, 
+				{ 1, 3, 3, 3, 3, 3, 3, 3, 1, 2 }, 
+				{ 1, 2, 3, 4, 1, 2, 3, 1, 1, 2 } 
+				};
+		char results[][] = new char[names.length][10];
+		int[] jumsus = new int[names.length];
+		for (int i = 0; i < results.length; i++) {
+			int jumsu=0;
+			for (int j = 0; j < results[i].length; j++) {
+				if (array[i][j] == answer[j]) {
+					results[i][j] = 'o';
+					jumsu+=10;
+				}
+					else
+					results[i][j] = 'x';
+			}
+			jumsus[i]=jumsu;
 		}
-
+		//////////////////////////////////////////////////////////
+		System.out.println("[채점결과]");
+		for(int i =0; i<names.length; i++) {
+			System.out.print(names[i]+" : ");
+			for(int j=0; j<results.length; j++) {
+				System.out.printf("%c \t",results[i][j]);
+			}
+			System.out.println("("+jumsus[i]+"점)");
+		}
 	}
 }
